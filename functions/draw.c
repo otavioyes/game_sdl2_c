380c27e5494d6b3f5bfa75e8496ef9246f8a6366
Otavio Correa <correaotavio0409@gmail.com>
2025-06-02 17:24:01 -0300

    player size-movement- bullets(wrong)

--- functions/draw.c
+++ functions/draw.c
@@ -1,25 +1,35 @@
 // draw.c
 #include "common.h"
 
+//prototipos
 static void drawPlayer(void);
 static void drawBullets(void);
 
-void prepareScene(void) {
-    SDL_SetRenderDrawColor(app.renderer, 46, 26, 71, 255);
-    SDL_RenderClear(app.renderer);
+//funcao de desenhar a tela-ROXO MEIA NOITE
+void prepareScene(void)
+{
+    SDL_SetRenderDrawColor(app.renderer, 46, 26, 71, 255);//define a cor de fundo
+    SDL_RenderClear(app.renderer); //limpa a tela com essa cor
 }
 
-void presentScene(void) {
+//atualiza a janela com tudo que foi desenhado no redenrizador
+//e como a cortina de um teatro, quando tudo tiver pronto, a cortina abre.
+void presentScene(void)
+{
     SDL_RenderPresent(app.renderer);
 }
 
-void draw(void) {
+//funcao que chama as funcoes de Stage.c
+void draw(void)
+{
     drawPlayer();
     drawBullets();
 }
 
+
 // carrega uma textura da imagem no arquivo
-SDL_Texture *loadTexture(char *filename) {
+SDL_Texture *loadTexture(char *filename)
+{
     SDL_Texture *texture = IMG_LoadTexture(app.renderer, filename);
     if (!texture) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "IMG_LoadTexture failed: %s", IMG_GetError());
@@ -28,16 +38,24 @@ SDL_Texture *loadTexture(char *filename) {
     return texture;
 }
 
+
 // desenha uma textura escalada
-void blit(SDL_Texture *texture, int x, int y, float scale) {
-    if (texture == NULL) {
+void blit(SDL_Texture *texture, int x, int y, float scale)
+{
+     if (texture == NULL)
+     {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "blit() called with NULL texture");
         return;
-    }
+     }
+
+
+    //redenrizando o tamanho da imagem.
     int w, h;
+
     SDL_QueryTexture(texture, NULL, NULL, &w, &h);
 
-    SDL_Rect dest = {
+    SDL_Rect dest =
+    {
         .x = x,
         .y = y,
         .w = (int)(w * scale),
@@ -48,12 +66,17 @@ void blit(SDL_Texture *texture, int x, int y, float scale) {
     SDL_RenderCopy(app.renderer, texture, NULL, &dest);
 }
 
-static void drawPlayer(void) {
-    blit(player->texture, player->x, player->y, 1.0f);
+//chama o blit e desenha o player nas posicoes X e Y e o tamanho correto.
+static void drawPlayer(void)
+{
+    blit(player->texture, player->x, player->y, player->scale);
 }
 
-static void drawBullets(void) {
-    for (Entity *b = stage.bulletHead.next; b != NULL; b = b->next) {
-        blit(b->texture, b->x, b->y, 1.0f);
+
+static void drawBullets(void)
+{
+    for (Entity *b = stage.bulletHead.next; b != NULL; b = b->next)
+    {
+        blit(b->texture, b->x, b->y, b->scale);
     }
 }
--- functions/stage.c
+++ functions/stage.c
@@ -1,152 +1,4 @@
 //stage.c
-/*
-#include "common.h"
-
-void initStage(void)
-{
-    app.delegate.logic = logic;
-    app.delegate.draw = draw;
-
-    memset(&stage, 0, sizeof(Stage));
-    stage.fighterTail = &stage.fighterHead;
-    stage.bulletTail = &stage.bulletHead;
-
-    initPlayer();
-
-    bulletTexture = loadTexture("gfx/playerBullet.png");
-
-}
-/*
-A função initStage configura uma série de coisas para jogar o jogo.
-Ela atribui a lógica do delegado e os ponteiros de desenho a duas
-funções estáticas de lógica e desenho próprias, e também prepara
-as listas vinculadas de lutadores e balas para uso. Em seguida, ela
-chama a função initPlayer . Este é mais ou menos o mesmo código
-que tínhamos em main.c , exceto que o objeto jogador agora está
-mallocado e adicionado à lista vinculada de lutadores. Também
-definimos w e h da entidade jogador (sua largura e altura) com base
-no tamanho da textura, chamando SDL_QueryTexture . Também pré-
-armazenamos a imagem da bala em bulletTexture , para que não
-fiquemos carregando isso (e desperdiçando memória) sempre que
-criarmos uma bala.
-*/
-/*
-static void initPlayer()
-{
-    player = malloc(sizeof(Entity));
-    memset(player, 0, sizeof(Entity));
-    stage.fighterTail->next = player;
-    stage.fighterTail = player;
-
-    player->x = 100;
-    player->y = 100;
-    player->texture = loadTexture("gfx/player1.png");
-    SDL_QueryTexture(player->texture, NULL, NULL, &player->width, &player->height);
-}
-
-
-static void logic(void)
-{
-    doPlayer();
-    doBUllet();
-}
-
-
-static void doPlayer(void)
-{
-    player->dx = player->dy = 0;
-
-    if(player->reload > 0)
-    {
-        player->reload--;
-    }
-
-    if(app.keyboard[SDL_SCANCODE_UP])
-    {
-        player->dy = -PLAYER_SPEED;
-    }
-
-    if(app.keyboard[SDL_SCANCODE_DOWN])
-    {
-        player->dy = PLAYER_SPEED;
-    }
-
-    if(app.keyboard[SDL_SCANCODE_LEFT])
-    {
-        player->dx = -PLAYER_SPEED;
-    }
-
-    if(app.keyboard[SDL_SCANCODE_RIGHT])
-    {
-        player->dx = -PLAYER_SPEED;
-    }
-
-    if(app.keyboard[SDL_SCANCODE_RIGHT])
-    {
-        player->dx = PLAYER_SPEED;
-    }
-
-    if(app.keyboard[SDL_SCANCODE_LCTRL] && player->reload == 0)
-    {
-        fireBullet();
-    }
-
-    player->x += player->dx;
-    player->y += player->dy;
-}
-
-static void fireBullet(void)
-{
-    Entity *bullet;
-
-    bullet = malloc(sizeof(Entity));
-    memset(bullet, 0, sizeof(Entity));
-    stage.bulletTail->next = bullet;
-    stage.bulletTail = bullet;
-
-    bullet->x = player->x;
-    bullet->y = player->y;
-    bullet->dx = PLAYER_BULLET_SPEED;
-    bullet->health = 1;
-    bullet->texture = bulletTexture;
-    SDL_QueryTexture(bullet->texture, NULL, NULL, &bullet->width, &bullet->height);
-
-    bullet->y += (player->height / 2) - (bullet->height / 2);
-
-    player->reload = 8;
-}
-
-
-static void doBullet(void)
-{
-
-    Entity *b, *prev;
-
-    prev = &stage.bulletHead;
-
-    for(b = stage.bulletHead.next; b != NULL; b = b->next)
-    {
-        b->x += b->dx;
-        b->y += b->dy;
-
-        if(b->x > SCREEN_WIDTH)
-        {
-            if(b == stage.bulletTail)
-            {
-                stage.bulletTail = prev;
-            }
-
-            prev->next = b->next;
-            free(b);
-            b = prev;
-        }
-        prev = b;
-    }
-}
-*/
-
-
-
 #include "common.h"
 
 // protótipos internos
@@ -155,7 +7,9 @@ static void doPlayer(void);
 static void doBullet(void);
 static void fireBullet(void);
 
-void initStage(void) {
+
+void initStage(void)
+{
     app.delegate.logic = logic;
     app.delegate.draw  = draw;
 
@@ -167,35 +21,115 @@ void initStage(void) {
     bulletTexture = loadTexture("gfx/playerBullet.png");
 }
 
-static void initPlayer(void) {
+static void initPlayer(void)
+{
     player = malloc(sizeof(Entity));
     memset(player, 0, sizeof(Entity));
     stage.fighterTail->next = player;
     stage.fighterTail = player;
 
-    player->x       = 100;
-    player->y       = 100;
+    player->x       = 100; //posicao X onde o player nasce
+    player->y       = 100; //posicao Y onde o player nasce
+    player->scale   = 0.1f;//tamanho da imagem /scale
     player->texture = loadTexture("gfx/player1.png");
     SDL_QueryTexture(player->texture, NULL, NULL,
                      &player->width, &player->height);
 }
 
-void logic(void) {
+
+void logic(void)
+{
     doPlayer();
     doBullet();
 }
 
-static void doPlayer(void) {
-    // …
+//funcao de  movimentacao do jogador
+static void doPlayer(void)
+{
+    player->dx = player->dy = 0;
+
+    if (player->reload > 0)
+    {
+        player->reload--;
+    }
+
+    if (app.keyboard[SDL_SCANCODE_UP])
+    {
+        player->dy = -PLAYER_SPEED;
+    }
+
+    if (app.keyboard[SDL_SCANCODE_DOWN])
+    {
+        player->dy = PLAYER_SPEED;
+    }
+
+    if (app.keyboard[SDL_SCANCODE_LEFT])
+    {
+        player->dx = -PLAYER_SPEED;
+    }
+
+    if (app.keyboard[SDL_SCANCODE_RIGHT])
+    {
+        player->dx = PLAYER_SPEED;
+    }
+
     if (app.keyboard[SDL_SCANCODE_LCTRL] && player->reload == 0)
+    {
         fireBullet();
-    // …
+    }
+
+    player->x += player->dx;
+    player->y += player->dy;
 }
 
-static void doBullet(void) {
-    // …
+
+static void doBullet(void)
+{
+    Entity *b, *prev;
+
+    prev = &stage.bulletHead;
+
+    for (b = stage.bulletHead.next ; b != NULL ; b = b->next)
+    {
+        b->x     += b->dx;
+        b->y     += b->dy;
+        b->scale += 0.1f;
+
+        if (b->x > SCREEN_WIDTH)
+        {
+            if (b == stage.bulletTail)
+            {
+                stage.bulletTail = prev;
+            }
+
+            prev->next = b->next;
+            free(b);
+            b = prev;
+        }
+
+        prev = b;
+    }
 }
 
-static void fireBullet(void) {
-    // …
+
+//funcao que chama valocidade das balas e faz atirar
+static void fireBullet(void)
+{
+    Entity *bullet;
+
+    bullet = malloc(sizeof(Entity));
+    memset(bullet, 0, sizeof(Entity));
+    stage.bulletTail->next = bullet;
+    stage.bulletTail = bullet;
+
+    bullet->x       = player->x;
+    bullet->y       = player->y;
+    bullet->dx = PLAYER_BULLET_SPEED;
+    bullet->health = 1;
+    bullet->texture = bulletTexture;
+    SDL_QueryTexture(bullet->texture, NULL, NULL, &bullet->width, &bullet->height);
+
+    bullet->y += (player->height / 2) - (bullet->height / 2);
+
+    player->reload = 8;
 }

154073c27be72aec04f20590b3ce28d6dcddce39
Otavio Correa <correaotavio0409@gmail.com>
2026-05-08 17:10:09 -0300

    comentado: bullet.h

--- include/bullet.h
+++ include/bullet.h
@@ -2,7 +2,14 @@
  * bullet.h
  */
 
+/* Cria um novo projétil */
 void fireBullet(SDL_Texture *texture);
+
+/* Atualiza lógica dos projéteis */
 void doBullet(void);
+
+/* Renderiza projéteis ativos */
 void drawBullets(void);
+
+/* Verifica colisão entre projétil e entidades */
 int bulletHitFighter(Entity *b);
